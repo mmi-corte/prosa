f2ac463db3082c23ffbe9d7d312a2648e9e83f7a
AlyciaWood <enora.tortech@gmail.com>
2025-01-23 11:39:44 +0100

    ajustement page 1

--- js/screen.js
+++ js/screen.js
@@ -11,13 +11,14 @@ export function loadScreen0(){
     console.log('loadScreen0: je suis là');
 
     // add background
-    document.body.style.background = "linear-gradient(149deg, rgba(230, 181, 122, 1) 0%, rgba(232, 188, 134, 1) 42%, rgba(245, 225, 202, 1) 100%)";
+    //document.body.style.background = "linear-gradient(149deg, rgba(230, 181, 122, 1) 0%, rgba(232, 188, 134, 1) 42%, rgba(245, 225, 202, 1) 100%)";
 
     // add logo
     addImg('container','../assets/logo.png','logoimg', 'logoImgP1');
+    addImgBackground('container',"assets/bg/Accueil.png")
 
     // add button
-    const btn1 = ajouterBouton('container', 'Commencez', 'btn1', 'btnclass');
+    const btn1 = ajouterBouton('container', 'JOUER MAINTENANT', 'btn1', 'btnclass');
 
     // binding
     btn1.addEventListener('click', function (){
@@ -53,6 +54,7 @@ export function loadScreen2(){
 
     // clear page
     refreshPage();
+    
 
     // update level dans cookie
     document.cookie = "screen=2; level=0; path=/;";
