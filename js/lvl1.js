b52d6c2fce1e337aa24aefe13993170a7efdf48c
Capocchi <lcapocchi@gmail.com>
2025-02-06 11:13:38 +0100

    add path foir naration and personnage - bug fix

--- js/lvl1.js
+++ js/lvl1.js
@@ -1,9 +1,14 @@
+// import
 import { loadLvl2 } from "./lvl2.js";
-
 import { playSteps } from "./functionMakeSteps.js";
 import { loadLvl1bis } from "./lvl1bis.js";
 import { loadSound, stopSound} from "./Sound/sound.js";
 import { log } from "./trace.js";
+
+// consts
+const path_sound = "assets/sound/";
+const path_personnages = "assets/personnages/";
+const path_narration = path_sound + "narration/";
 
 export function loadLvl1() {
 
@@ -14,25 +19,25 @@ export function loadLvl1() {
     
     // Liste des étapes du niveau 1
     const steps = [
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra", character: null , sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-001.mp3'},
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger", character: 'assets/personnages/Berger/berger.png', name: 'E1Berger' , sound: 'asset/sound/narration/Berger-E1/Berger-E1-001.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra1", character: null , sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-003.mp3'},
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger1", character: 'assets/personnages/Berger/berger.png', name: 'E1Berger1' , sound: 'asset/sound/narration/Berger-E1/Berger-E1-002.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra2", character: null , sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-004.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger2", character: 'assets/personnages/Berger/berger.png', name: 'E1Berger2' , sound: 'asset/sound/narration/Berger-E1/Berger-E1-003.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra3", character: null , sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-005.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger3", character: 'assets/personnages/Berger/berger.png', name: 'E1Berger3' , sound: 'asset/sound/narration/Berger-E1/Berger-E1-004.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra4", character: null , sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-006.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger4", character: 'assets/personnages/Berger/berger.png', name: 'E1Berger4' , sound: 'asset/sound/narration/Berger-E1/Berger-E1-005.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra5", character: null , sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-007.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger5", character: 'assets/personnages/Berger/berger.png', name: 'E1Berger5' , sound: 'asset/sound/narration/Berger-E1/Berger-E1-006.mp3'},
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra6", character: null , sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-008.mp3' },
-        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger6", character: 'assets/personnages/Berger/berger.png', name: 'E1Berger6' , sound: 'asset/sound/narration/Berger-E1/Berger-E1-007.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra", character: null , sound: path_narration+"Narrateur-E1/narrateurE1-001.mp3"},
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger", character: path_personnages+'Berger/berger.png', name: 'E1Berger' , sound: path_narration+"Berger-E1/Berger-E1-001.mp3" },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra1", character: null , sound: path_narration+'Narrateur-E1/narrateurE1-003.mp3'},
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger1", character: path_personnages+'Berger/berger.png', name: 'E1Berger1' , sound: path_narration+"Berger-E1/Berger-E1-002.mp3" },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra2", character: null , sound: path_narration+'Narrateur-E1/narrateurE1-004.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger2", character: path_personnages+'Berger/berger.png', name: 'E1Berger2' , sound: path_narration+"Berger-E1/Berger-E1-003.mp3" },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra3", character: null , sound: path_narration+'Narrateur-E1/narrateurE1-005.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger3", character: path_personnages+'Berger/berger.png', name: 'E1Berger3' , sound: path_narration+'Berger-E1/Berger-E1-004.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra4", character: null , sound: path_narration+'Narrateur-E1/narrateurE1-006.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger4", character: path_personnages+'Berger/berger.png', name: 'E1Berger4' , sound: path_narration+'Berger-E1/Berger-E1-005.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra5", character: null , sound: path_narration+'Narrateur-E1/narrateurE1-007.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger5", character: path_personnages+'Berger/berger.png', name: 'E1Berger5' , sound: path_narration+'Berger-E1/Berger-E1-006.mp3'},
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Narra6", character: null , sound: path_narration+'Narrateur-E1/narrateurE1-008.mp3' },
+        { background: 'assets/bg/fondEtape1.mp4', narration: "E1Berger6", character: path_personnages+'Berger/berger.png', name: 'E1Berger6' , sound: path_narration+'Berger-E1/Berger-E1-007.mp3' },
         {
             background: 'assets/bg/fondEtape1.mp4',
             narration: "E1Narra7",
             character: null,
-            sound: 'assets/sound/narration/Narrateur-E1/narrateurE1-009.mp3',
+            sound: path_narration+'Narrateur-E1/narrateurE1-009.mp3',
             choices: [
                 { text: "E1Choix1", action: loadLvl1bis },
                 { text: "E1Choix2", action: loadLvl2 }
