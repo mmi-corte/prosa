e79c3cdbdabd2bf4fb8f691840dea9b2dfc9b030
florianberaud <beraudflorian6@gmail.com>
2025-02-09 22:10:35 +0100

    addsound lvl4-5

--- js/lvl2.js
+++ js/lvl2.js
@@ -8,6 +8,29 @@ export function loadLvl2() {
 
     // Trace the entry in the level 2
     log("Enter in L2", "blue");
+    playSteps(mapData, 0, false, 2);
+    const steps =[
+        {character : "NaraChara", Txt : "E2Narra" , sound: 'assets/sound/narration/Narrateur-E2/narrateurE2-001.mp3'},
+        {character : "BergerChara", Txt : "E2Berger", name : "E2Berger" , sound: 'assets/sound/narration/Berger-E2/Berger-E2-001mp3'},
+        {character : "NaraChara", Txt : "E2Narra2" , sound: 'assets/sound/narration/Narrateur-E2/narrateurE2-002.mp3'},
+        {character : "FataChara", Txt : "E2Fata", name : "E2Fata" , sound: 'assets/sound/narration/FataE2/Fata-E2-001.mp3'},
+        {character : "EsterelleChara", Txt : "E2Esterelle",name : "E2Esterelle" , sound: 'assets/sound/narration/EsterelleE2/Esterelle-E2-001.mp3'},
+        {character : "NaraChara", Txt : "E2Narra3", sound: 'assets/sound/narration/Narrateur-E2/narrateurE2-003.mp3'},
+        {character : "EsterelleChara", Txt : "E2Esterelle2", name : "E2Esterelle2" , sound: 'assets/sound/narration/EsterelleE2/Esterelle-E2-002.mp3'},
+        {character : "FataChara", Txt : "E2Fata2", name : "E2Fata2" , sound: 'assets/sound/narration/FataE2/Fata-E2-002.mp3'},
+        {character : "EsterelleChara", Txt : "E2Esterelle3", name : "E2Esterelle3" , sound: 'assets/sound/narration/EsterelleE2/Esterelle-E2-003.mp3'},
+        {character : "FataChara", Txt : "E2Fata3", name : "E2Fata3" , sound: 'assets/sound/narration/FataE2/Fata-E2-003.mp3'},
+        {character : "NaraChara", Txt : "E2Narra4" , sound: 'assets/sound/narration/Narrateur-E2/narrateurE2-004.mp3'},
+        {character : "FataChara", Txt : "E2Fata4", name : "E2Fata4" , sound: 'assets/sound/narration/FataE2/Fata-E2-004.mp3'},
+        {character : "EsterelleChara", Txt : "E2Esterelle4", name : "E2Esterelle4" , sound: 'assets/sound/narration/EsterelleE2/Esterelle-E2-004.mp3'},
+        {character : "FataChara", Txt : "E2Fata5", name : "E2Fata5" , sound: 'assets/sound/narration/FataE2/Fata-E2-005.mp3'},
+        {character : "NarraChara", Txt : "E2Narra5", sound: 'assets/sound/narration/Narrateur-E2/narrateurE2-005.mp3'},
+        {character : "EsterelleChara", Txt : "E2Esterelle5", name : "E2Esterelle5" , sound: 'assets/sound/narration/EsterelleE2/Esterelle-E2-005.mp3'},
+        {character : "FataChara", Txt : "E2Fata6",name : "E2Fata6" , sound: 'assets/sound/narration/FataE2/Fata-E2-006.mp3'},
+        {character : "NaraChara", Txt : "E2Narra6" , sound: 'assets/sound/narration/Narrateur-E2/narrateurE2-006.mp3'},
+        {character : "BergerChara", Txt : "E2Berger2", name : "E2Berger2" , sound: 'assets/sound/narration/Berger-E2/Berger-E2-002mp3'},
+        {character : "NaraChara", Txt : "E2Narra" , sound: 'assets/sound/narration/Narrateur-E2/narrateurE2-006.mp3'},
+        ] 
 
     // Set the level in localStorage
     localStorage.setItem("level", "2"); 
