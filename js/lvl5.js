98308536a3f596a44b64d69384e4df1cd921a566
florianberaud <beraudflorian6@gmail.com>
2025-02-10 10:40:51 +0100

    addsonbergerE1

--- js/lvl5.js
+++ js/lvl5.js
@@ -13,19 +13,19 @@ export function loadLvl5() {
 
     // Refresh the page
     const steps =[
-        {character: "NarraChara", Txt: "E5Narra", sound: path_narration+'Narrateur-E5/naratteurE5-001.mp3'},
+        {character: "NarraChara", Txt: "E5Narra", sound: path_narration+'Narrateur-E5/NarateurE5-001.mp3'},
         {character: "BergerChara", Txt: "E5Berger", name: "E5Berger", sound: path_narration+'Berger-E5/Berger-E5-001.mp3'},
-        {character: "NarraChara", Txt: "E5Narra2", sound: path_narration+'Narrateur-E5/naratteurE5-002.mp3'},
+        {character: "NarraChara", Txt: "E5Narra2", sound: path_narration+'Narrateur-E5/NarateurE5-002.mp3'},
         {character: "MascoChara", Txt: "E5Masco", name: "E5Masco", sound: path_narration+'Masco-E5/mascoE5-001.mp3'},
-        {character: "NarraChara", Txt: "E5Narra3", sound: path_narration+'Narrateur-E5/naratteurE5-003.mp3'},
+        {character: "NarraChara", Txt: "E5Narra3", sound: path_narration+'Narrateur-E5/NarateurE5-003.mp3'},
         {character: "MascoChara", Txt: "E5Masco2", name: "E5Masco2", sound: path_narration+'Masco-E5/mascoE5-002.mp3'},
-        {character: "NarraChara", Txt: "E5Narra4", name: "E5Narra4", sound: path_narration+'Narrateur-E5/naratteurE5-004.mp3'},
+        {character: "NarraChara", Txt: "E5Narra4", name: "E5Narra4", sound: path_narration+'Narrateur-E5/NarateurE5-004.mp3'},
         {character: "MascoChara", Txt: "E5Masco3", name: "E5Masco3", sound: path_narration+'Masco-E5/mascoE5-003.mp3'},
-        {character: "NarraChara", Txt: "E5Narra5", sound: path_narration+'Narrateur-E5/naratteurE5-005.mp3'},
+        {character: "NarraChara", Txt: "E5Narra5", sound: path_narration+'Narrateur-E5/NarateurE5-005.mp3'},
         {character: "MascoChara", Txt: "E5Masco4", name: "E5Masco4", sound: path_narration+'Masco-E5/mascoE5-004.mp3'},
-        {character: "NarraChara", Txt: "E5Narra6", sound: path_narration+'Narrateur-E5/naratteurE5-006.mp3'},
+        {character: "NarraChara", Txt: "E5Narra6", sound: path_narration+'Narrateur-E5/NarateurE5-006.mp3'},
         {character: "BergerChara", Txt: "E5Berger4", name: "E5Berger4", sound: path_narration+'Berger-E5/Berger-E5-002.mp3'},
-        {character: "NarraChara", Txt: "E5Narra7", sound: path_narration+'Narrateur-E5/naratteurE5-007.mp3'}
+        {character: "NarraChara", Txt: "E5Narra7", sound: path_narration+'Narrateur-E5/NarateurE5-007.mp3'}
     ]
     
     // Play the steps
