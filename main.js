ec0d447918528e360a48a3eee6756ba1bdb75f97
Capocchi <lcapocchi@gmail.com>
2025-02-11 17:18:41 +0100

    final test for pwa

--- main.js
+++ main.js
@@ -169,11 +169,11 @@ loadJSON();
 
 // });
 
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", function() {
-//     navigator.serviceWorker
-//       .register("/sw.js")
-//       .then(res => console.log("service worker registered"))
-//       .catch(err => console.log("service worker not registered", err));
-//   });
-// }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
