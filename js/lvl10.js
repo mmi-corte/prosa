ca6291390be41dbddab7fc4f6885ecd9ea386d52
Capocchi <lcapocchi@gmail.com>
2025-04-19 00:09:38 +0200

    first update level10

--- js/lvl10.js
+++ js/lvl10.js
@@ -18,24 +18,24 @@ export function loadLvl10() {
 
     const steps = [
         { character: "NarraChara", Txt: "E10Narra", sound: path_narration+'Narrateur-E10/narrateurE10-001.mp3' },
-        { character: "BergerChara", Txt: "E10Berger", name: "E10Berger" },
+        { character: "BergerChara", Txt: "E10Berger", name: "E10Berger", sound: path_narration+'Berger-E10/bergerE10-001.mp3' },
         { character: "NarraChara", Txt: "E10Narra2", sound: path_narration+'Narrateur-E10/narrateurE10-002.mp3'},
-        { character: "OrcuChara", Txt: "E10Orcu", name: "E10Orcu" }
+        { character: "OrcuChara", Txt: "E10Orcu", name: "E10Orcu", sound: path_narration+'Orcu-E10/Orcu-E10-005.mp3' }
     ];
 
     //Viens après les fées ?
     if(localStorage.getItem("fairy")) {
-        steps.push( { character: "NarraChara", Txt: "E10NarraAFairy", name: "E10NarraAFairy"},
-                    { character: "OrcuChara", Txt: "E10OrcuAFairy", name: "Orcus" });
+        steps.push( { character: "NarraChara", Txt: "E10NarraAFairy", name: "E10NarraAFairy", sound: path_narration+'Narrateur-E10/narrateurE10-005.mp3'},
+                    { character: "OrcuChara", Txt: "E10OrcuAFairy", name: "Orcus", sound: path_narration+'Orcu-E10/Orcu-E10-006.mp3' });
     }
 
     if (localStorage.getItem("lion")) {
-        steps.push( { character: "NarraChara", Txt: "E10NarraALeoS", name: "E10NarraALeoS"},
-                    { character: "OrcuChara", Txt: "E10OrcuALeoS", name: "E10OrcuALeoS" });
+        steps.push( { character: "NarraChara", Txt: "E10NarraALeoS", name: "E10NarraALeoS", sound: path_narration+'Narrateur-E10/narrateurE10-006.mp3'},
+                    { character: "OrcuChara", Txt: "E10OrcuALeoS", name: "E10OrcuALeoS", sound: path_narration+'Orcu-E10/Orcu-E10-005.mp3' });
     }
 
-    steps.push({ character: "NarraChara", Txt: "E10Narra3"},
-            { character: "OrcuChara", Txt: "E10Orcu2", name: "E10Orcu2" },
+    steps.push({ character: "NarraChara", Txt: "E10Narra3", sound: path_narration+'Narrateur-E10/narrateurE10-007.mp3'},
+            { character: "OrcuChara", Txt: "E10Orcu2", name: "E10Orcu2", sound: path_narration+'Orcu-E10/Orcu-E10-007.mp3' },
             { character: "BergerChara", Txt: "E10Berger2", name: "E10Berger2" },
             { character: "OrcuChara", Txt: "E10Orcu3", name: "E10Orcu3" },
             { character: "NarraChara", Txt: "E10Narra4", popup: "E8Mess"},
