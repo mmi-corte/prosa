14b7cb83d194b883faa9566bca0b5109489574b8
depmmi <depmmi@univ-corse.fr>
2025-11-27 17:24:37 +0100

    new functions

--- js/main.js
+++ js/main.js
@@ -3,6 +3,27 @@ import { ARSystem } from './systems/ARSystem.js';
 import { loadSceneContent } from './content/SceneContent.js';
 
 
+function createMesh() {
+  // 3. Contenu 3D (Géométrie & Matériaux)
+  const geometry = new THREE.PlaneGeometry(1, 0.55);
+  const material = new THREE.MeshBasicMaterial({ 
+    color: 0x00ffff, 
+    transparent: true, 
+    opacity: 0.5 
+  });
+  const obj = new THREE.Mesh(geometry, material);
+
+  return obj
+}
+
+function create2Dobject(src) {
+  // code to load the 2D image with a THREE Obj
+}
+
+function create3Dobject(src) {
+  // code to load the 3D obj with a THREE Obj
+}
+
 const main = async () => {
   
   // --- CONFIGURATION ---
@@ -11,18 +32,12 @@ const main = async () => {
   const targetUrl = "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/card-example/card.mind";
   // ---------------------
 
-  // 3. Contenu 3D (Géométrie & Matériaux)
-  const geometry = new THREE.PlaneGeometry(1, 0.55);
-  const material = new THREE.MeshBasicMaterial({ 
-    color: 0x00ffff, 
-    transparent: true, 
-    opacity: 0.5 
-  });
-  const plane = new THREE.Mesh(geometry, material);
+  
 
   // 2. Charger la scène en passant l'URL
   // Ordre des paramètres : (Selecteur, URL, Système)
-  loadSceneContent(targetUrl, plane, containerId);
+  // 
+  loadSceneContent(targetUrl, create2Dobject("./assets/img/00.png"), containerId);
 
   
 };
