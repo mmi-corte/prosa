468cabab87a51465d4a213b34a8a84fc372bf9b3
Alex.D <alxcorp.pro@gmail.com>
2025-11-27 17:47:19 +0100

    update PWA

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,7 @@ const ASSETS_TO_CACHE = [
   './css/style.css',   // Corrigé (était ./style.css)
   './js/main.js',      // Corrigé (était ./script.js)
   './manifest.json',
+  './assets',
   // './assets/mon-modele.glb', // DÉCOMMENTEZ SI LE FICHIER EXISTE VRAIMENT
   
   // NOTE : J'ai retiré three.js du cache ici car vous l'importez via un CDN dans l'importmap
