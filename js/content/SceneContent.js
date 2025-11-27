4ddef313d1e4b2260787103ad6f966d800e16b8b
depmmi <depmmi@univ-corse.fr>
2025-11-27 16:49:26 +0100

    update

--- js/content/SceneContent.js
+++ js/content/SceneContent.js
@@ -1,23 +1,15 @@
 import * as THREE from 'three';
+import { ARSystem } from './systems/ARSystem.js';
 
 // On ajoute 'targetUrl' dans les paramètres reçus
-export const loadSceneContent = (containerSelector, targetUrl, arSystem) => {
+export const loadSceneContent = (targetUrl, plane, containerId) => {
   
-  // 1. Initialisation avec l'URL reçue depuis le main.js
-  arSystem.init(containerSelector, targetUrl);
-
-  // 2. Création de l'ancre
-  const anchor = arSystem.addAnchor(0);
-
-  // 3. Contenu 3D (Géométrie & Matériaux)
-  const geometry = new THREE.PlaneGeometry(1, 0.55);
-  const material = new THREE.MeshBasicMaterial({ 
-    color: 0x00ffff, 
-    transparent: true, 
-    opacity: 0.5 
-  });
-  const plane = new THREE.Mesh(geometry, material);
-  
+   // 1. Instancier le système
+   const arSystem = new ARSystem();
+   arSystem.init(containerId, targetUrl);
+ 
+   // 2. Création de l'ancre
+   const anchor = arSystem.addAnchor(0);
   // 4. Ajout à la scène
   anchor.group.add(plane);
 };
--- js/main.js
+++ js/main.js
@@ -9,12 +9,18 @@ const main = async () => {
   const targetUrl = "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/card-example/card.mind";
   // ---------------------
 
-  // 1. Instancier le système
-  const arSystem = new ARSystem();
+    // 3. Contenu 3D (Géométrie & Matériaux)
+  const geometry = new THREE.PlaneGeometry(1, 0.55);
+  const material = new THREE.MeshBasicMaterial({ 
+    color: 0x00ffff, 
+    transparent: true, 
+    opacity: 0.5 
+  });
+  const plane = new THREE.Mesh(geometry, material);
 
   // 2. Charger la scène en passant l'URL
   // Ordre des paramètres : (Selecteur, URL, Système)
-  loadSceneContent(containerId, targetUrl, arSystem);
+  loadSceneContent(targetUrl, plane, containerId);
 
   // 3. UI
   initUI({
